434cfaa836f8329785453ae345d2599c7bdf2946
Angga Aditya <agungangga2001@gmail.com>
2024-01-16 17:45:59 +0800

    offer done need edit

--- src/component/slider/Content.js
+++ src/component/slider/Content.js
@@ -1,62 +1,136 @@
 import React from 'react'
-import Slider from './Slider';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", color: "red" }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", color: "red" }}
+      onClick={onClick}
+    />
+  );
+}
 
 export default function Content() {
-  const slides = [
-    [
-      {
-        title: 'Slide 1',
-        description: 'This is the first slide for Slider 1.',
-        image: './assets/image1.png',
-      },
-      {
-        title: 'Slide 2',
-        description: 'This is the second slide for Slider 1.',
-        image: './assets/image2.png',
-      },
-      {
-        title: 'Slide 3',
-        description: 'This is the third slide for Slider 1.',
-        image: './assets/image3.png',
-      },
-      {
-        title: 'Slide 1',
-        description: 'This is the first slide for Slider 1.',
-        image: './assets/image1.png',
-      },
-      {
-        title: 'Slide 2',
-        description: 'This is the second slide for Slider 1.',
-        image: './assets/image2.png',
-      },
-      {
-        title: 'Slide 3',
-        description: 'This is the third slide for Slider 1.',
-        image: './assets/image3.png',
-      },
-      {
-        title: 'Slide 1',
-        description: 'This is the first slide for Slider 1.',
-        image: './assets/image1.png',
-      },
-      {
-        title: 'Slide 2',
-        description: 'This is the second slide for Slider 1.',
-        image: './assets/image2.png',
-      },
-      {
-        title: 'Slide 3',
-        description: 'This is the third slide for Slider 1.',
-        image: './assets/image3.png',
-      },
-    ],
-  ];
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+  };
+  const data = [
+    {
+      title: 'Slide 1',
+      description: 'This is the first slide for Slider 1.',
+      image: './assets/image1.png',
+    },
+    {
+      title: 'Slide 2',
+      description: 'This is the second slide for Slider 1.',
+      image: './assets/image2.png',
+    },
+    {
+      title: 'Slide 3',
+      description: 'This is the third slide for Slider 1.',
+      image: './assets/image3.png',
+    },
+    {
+      title: 'Slide 4',
+      description: 'This is the first slide for Slider 1.',
+      image: './assets/image1.png',
+    },
+    {
+      title: 'Slide 5',
+      description: 'This is the second slide for Slider 1.',
+      image: './assets/image2.png',
+    },
+    {
+      title: 'Slide 6',
+      description: 'This is the third slide for Slider 1.',
+      image: './assets/image3.png',
+    },
+    {
+      title: 'Slide 7',
+      description: 'This is the first slide for Slider 1.',
+      image: './assets/image1.png',
+    },
+    {
+      title: 'Slide 8',
+      description: 'This is the second slide for Slider 1.',
+      image: './assets/image2.png',
+    },
+    {
+      title: 'Slide 9',
+      description: 'This is the third slide for Slider 1.',
+      image: './assets/image3.png',
+    },
+  ]
+
 
   return (
-    <div className="App">
-      {slides.map((slideContent, index) => (
-        <Slider key={index} slides={slideContent} />
-      ))}
+    <div class='w-3/4 m-auto'>
+      <div className='mt-20'>
+      <Slider {...settings}>
+        {data.map ((d) => (
+          <div className='bg-white h-[450px] text-black rounded-xl'>
+            <div className='h-56 flex justify-center items-center'>
+              <img src={d.image} alt='' className='h-full w-full'/>
+            </div>
+
+            <div className='flex flex-col justify-center items-center gap-4 p-4 border-2 rounded-b-xl'>
+              <p className='text-xl font-bold'>{d.title}</p>
+              <p>{d.description}</p>
+              <button className='bg-VividRed text-white text-lg px-6 py-1 rounded-xl'>Read More</button>
+            </div>
+          </div>
+        ))}
+      </Slider>
+      </div>
+
     </div>
   )
 }
--- src/view/landingPage/LandingPage.js
+++ src/view/landingPage/LandingPage.js
@@ -12,6 +12,25 @@ export default function LandingPage() {
         <ShrinkingNavbar/>
         <SearchInput/>
         <Content/>
+
+        <div class='flex items-center justify-center'>
+          <div class='flex flex-row'>
+            <div className='pt-11 md:pt-9 lg:pt-16'>
+              <img src='./assets/Barong.png' alt='barong' class='p-2 w-20 h-20 md:w-40 md:h-40 lg:h-full lg:w-full lg:px-10'/> 
+            </div>
+            
+            <div class='px-1 md:px-5 lg:px-10 lg:pt-32'>
+              <p class='font-poppins font-bold text-sm pt-6 md:text-xl lg:text-3xl '>Why Choose Bali Cemerlang Tour?</p>
+              <p class='font-poppins text-xs pt-1 md:text-xl lg:text-xl'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <p class='font-poppins text-xs pt-1 md:text-xl lg:text-xl'>✔️ Best price range</p>
+              <p class='font-poppins text-xs pt-1 md:text-xl lg:text-xl'>✔️ Experienced for more than 10 years</p>
+              <p class='font-poppins text-xs pt-1 md:text-xl lg:text-xl'>✔️ A variety of accommodations to choose from</p>
+              <button class='mt-2 h-7 w-24 md:h-12 md:w-36 md:text-lg lg:mt-6 lg:h-14 lg:w-44 lg:text-xl bg-VividRed rounded-full font-poppins text-xs text-white'>
+                  About Us
+              </button>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
