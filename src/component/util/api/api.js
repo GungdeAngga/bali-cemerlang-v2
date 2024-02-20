977312c0384fa4d2d3150598eca9c4571c5f3461
vidinararia <vidinararia@gmail.com>
2024-02-20 14:18:04 +0800

    update url

--- src/component/util/api/api.js
+++ src/component/util/api/api.js
@@ -1,1 +1,34 @@
-export const baseApi = "http://maiharta.ddns.net:6969/api/";
+import axios from "axios";
+
+const baseApi = "http://localhost:8000/api/";
+
+// for getting image
+export const baseStorage = "http://localhost:8000/storage/";
+
+// axios function
+export const getPackage = () =>
+  axios({
+    method: "get",
+    url: baseApi + "package",
+    params: {
+      order_direction: "desc",
+    },
+  }).then(({ data }) => data.data);
+
+export const getOffer = () =>
+  axios({
+    method: "get",
+    url: baseApi + "offer",
+    params: {
+      order_direction: "desc",
+    },
+  }).then(({ data }) => data.data);
+
+export const getPromo = () =>
+  axios({
+    method: "get",
+    url: baseApi + "promo",
+    params: {
+      order_direction: "desc",
+    },
+  }).then(({ data }) => data.data);
